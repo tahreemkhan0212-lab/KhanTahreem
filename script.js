5c6e6ef59cd6d4ffdf3160b7dff6f0aea37f2bb0
tahreemkhan0212-lab <tahreemkhan0212@gmail.com>
2025-11-11 11:40:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for reaching out! I’ll get back to you soon 😊");
+  this.reset();
+});
